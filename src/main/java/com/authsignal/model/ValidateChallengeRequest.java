652e18d0c6d67a162731615afd63eda68122f59c
Chris Fisher <chris.i.fisher@gmail.com>
2024-04-05 12:51:26 +1300

    Use token validation endpoint

--- src/main/java/com/authsignal/model/ValidateChallengeRequest.java
+++ src/main/java/com/authsignal/model/ValidateChallengeRequest.java
@@ -2,5 +2,4 @@ package com.authsignal.model;
 
 public class ValidateChallengeRequest {
   public String token;
-  public String userId;
 }
--- src/main/java/com/authsignal/model/ValidateChallengeResponse.java
+++ src/main/java/com/authsignal/model/ValidateChallengeResponse.java
@@ -1,8 +1,14 @@
 package com.authsignal.model;
 
+import com.google.gson.annotations.SerializedName;
+
 public class ValidateChallengeResponse {
-  public Boolean success;
+  public Boolean isValid;
   public UserActionState state;
+  public String stateUpdatedAt;
   public String userId;
+  @SerializedName(value = "actionCode")
   public String action;
+  public String idempotencyKey;
+  public String verificationMethod;
 }
