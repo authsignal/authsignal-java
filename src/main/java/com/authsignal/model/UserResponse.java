b2379bd389777e1bf3d4ff20da96d911b0b9a996
justinsoong <justinsoong@gmail.com>
2024-09-23 11:39:33 +1200

    [AUT-2319] - Java SDK Alignment

--- src/main/java/com/authsignal/model/UserResponse.java
+++ src/main/java/com/authsignal/model/UserResponse.java
@@ -2,5 +2,10 @@ package com.authsignal.model;
 
 public class UserResponse {
   public String userId;
+  public String email;
+  public String phoneNumber;
   public Boolean isEnrolled;
+  public VerificationMethodType[] allowedVerificationMethods
+  public VerificationMethodType[] allowedVerificationMethods
+  public VerificationMethodType defaultVerificationMethod
 }
---
+++ src/main/java/com/authsignal/model/VerificationMethodType.java
@@ -0,0 +1,38 @@
+package com.authsignal.model;
+
+import com.google.gson.annotations.SerializedName;
+
+public enum VerificationMethodType {
+  @SerializedName("SMS")
+  SMS,
+
+  @SerializedName("AUTHENTICATOR_APP")
+  AUTHENTICATOR_APP,
+
+  @SerializedName("EMAIL_MAGIC_LINK")
+  EMAIL_MAGIC_LINK,
+
+  @SerializedName("EMAIL_OTP")
+  EMAIL_OTP,
+
+  @SerializedName("PUSH")
+  PUSH,
+
+  @SerializedName("SECURITY_KEY")
+  SECURITY_KEY,
+
+  SerializedName("PASSKEY")
+  PASSKEY,
+
+  SerializedName("VERIFF")
+  VERIFF,
+
+  SerializedName("IPROOV")
+  IPROOV,
+
+  SerializedName("PALM_BIOMETRICS_RR")
+  PALM_BIOMETRICS_RR,
+
+  SerializedName("IDVERSE")
+  IDVERSE,
+}
