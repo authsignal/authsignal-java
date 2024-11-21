713c1a1b9caa16c82adaf9d6eda02ab8e5c302d9
Chris Fisher <chris.i.fisher@gmail.com>
2024-11-22 10:27:29 +1300

    wip

--- src/main/java/com/authsignal/exception/InvalidURLException.java
+++
@@ -1,7 +0,0 @@
-package com.authsignal.exception;
-
-public class InvalidURLException extends AuthsignalException {
-    public InvalidURLException() {
-        super(500, "invalid_url", "The URL format is invalid.");
-    }
-}
