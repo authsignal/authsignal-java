613c118352d68424c625d567d2486f42a24465f6
Chris Fisher <chris.i.fisher@gmail.com>
2024-12-09 09:35:38 +1300

    Make api models implement serializable (#18)

--- src/main/java/com/authsignal/model/APIModel.java
+++ src/main/java/com/authsignal/model/APIModel.java
@@ -1,8 +1,10 @@
 package com.authsignal.model;
 
+import java.io.Serializable;
+
 import com.google.gson.Gson;
 
-class ApiModel {
+class ApiModel implements Serializable {
     public String toString() {
         return new Gson().toJson(this);
     }
